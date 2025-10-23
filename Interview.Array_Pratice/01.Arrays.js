308b1f3d6e31dc6a8d982d811d590588d1ffdd8c
Arpansaha-Devops <arpansaha0707@gmail.com>
2025-10-23 23:13:33 +0530

    array_day.01

---
+++ Interview.Array_Pratice/01.Arrays.js
@@ -0,0 +1,74 @@
+// let arr = [1,2,3,4,5,6,7,8];
+
+// // Method 1: Using array length
+// let lastElement = arr[arr.length - 1];
+
+// let arr =  [1,2,3,4,5,67,78];
+
+// console.log(arr.splice(5,1, 655))
+// console.log(arr)
+
+
+// let arr = [1,2,3,4,5,67,8,90];
+
+// arr.splice(0,arr.length)
+// console.log(arr)  // empty array method  -
+
+
+// let arr = [1,2,3,4,5,6,7,8]
+
+// for (const key of arr) {
+//     console.log(`${key *2}`)
+// }
+
+// let newArr = arr.findLast((key) => key % 2 == 0);
+// console.log(newArr)
+
+
+// let arr = [1,2,3,4,5,5];
+
+// let newARR = arr.filter((key ) => ( key > 2))
+
+// console.log(newARR)
+
+// let arr = [1, 2, 3, 4, 5, 5];
+// console.log(arr.includes(3)); // true , this method is use for finding the element in the array -
+ 
+
+// let arr = [1,2,3,4,5,6,78,88]
+
+// console.log(arr.find((key => key === 88)))
+
+// let arr = [1,2,3,4,5,6,78,88]
+
+// console.log(arr.reduce((acc,crr) => acc + crr),0);
+
+// console.log(arr.map(key => key * 2))
+
+// let arr = [90,80,70,60,50,40,30,20,10]
+
+// console.log(arr.sort((a, b) => a - b))
+
+
+
+// function reverseString(arr) {
+//     let reversed = [];
+//     for (const char of arr) {
+//         reversed.unshift(char); // Using unshift instead of push to add elements at the beginning
+//     }
+//     return reversed;
+// }
+
+// console.log(reverseString([1,2,3,4,5,6,7,8,9,10]))
+// console.log(typeof reverseString([1,2,3,4,5,6,7,8,9,10]))
+
+//  let asrr = [1,2,3,4,5,6,67,8]
+
+//  console.log(asrr.reverse())
+
+
+// let arr = [1,2,3,3,4,5,5,6];
+// let uniqueArr = [...new Set(arr)];
+// console.log(uniqueArr); // [1,2,3,4,5,6]
+
+
