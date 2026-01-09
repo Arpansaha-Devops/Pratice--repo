ecc7c5f3f95e29a06280c43c1d77f58049b00715
Arpan <arpansaha0707@gmail.com>
2026-01-09 23:02:41 +0530

    new question solve

--- Js PraticeDay 1/Day3.js
+++ Js PraticeDay 1/Day3.js
@@ -1,16 +1,30 @@
-const arr = [24,57,21,45,32,67];
-const target = 45;
+// const arr = [24,57,21,45,32,67];
+// const target = 45;
 
-function twoSum(arr,target) {
-    const map = new Map();
-    for (let index = 0; index < arr.length; index++) {
+// function twoSum(arr,target) {
+//     const map = new Map();
+//     for (let index = 0; index < arr.length; index++) {
         
-        const complete = target - arr[index]
-        if (map.has(complete)) {
-            return [map.get(complete),index]
-        }
-        map.set(arr[index],index)
-    }
-}
+//         const complete = target - arr[index]
+//         if (map.has(complete)) {
+//             return [map.get(complete),index]
+//         }
+//         map.set(arr[index],index)
+//     }
+// }
 
-console.log(twoSum(arr,target))
+// console.log(twoSum(arr,target))
+
+
+// function duplicAtearr (arr){
+//    const newArr = arr.filter((item,index,array) => array.indexOf(item) === index )
+//    return newArr === arr
+// }
+
+// console.log(duplicAtearr([1,2,3,3,4,5,5]))
+
+const arr = [1,2,3,4,5,6]
+
+const newArr = [...new Set(arr)]
+
+console.log(newArr.length !== arr.length)
