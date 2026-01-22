f0f607883f0dba2660154315424a3c962aae5990
Arpan <arpansaha0707@gmail.com>
2026-01-23 03:46:08 +0530

    Tricky questions

---
+++ Js PraticeDay 1/Day6.js
@@ -0,0 +1,73 @@
+'use strict';
+
+// let a = 12;
+// console.log(a * 12)
+// console.log(a ** 12)
+
+
+
+// function test() {
+//    console.log(test.abc)
+// }
+
+// test()
+// test.abc = 400
+// test.abc = 600
+
+// test()
+
+// let a ={
+//     name : "arpan"
+// }
+
+// let b = {
+//     ...a
+// }
+
+// b.name = "tanmoy"
+
+// console.log(a.name)
+
+
+// let a = 10;
+// let b = new Number(10)
+// let c = 10;
+
+// console.log(a === b) // false
+// console.log(b === c) // false
+// console.log(typeof b) // object 
+
+
+// function test(record) {
+//     if (record == {age : 18}) {
+//         console.log("you are an adult")
+//     }else if (record === {age : 22}) {
+//         console.log("you are still an adult")
+//     } else{
+//         console.log("no record")
+//     }
+// }
+
+// test(record = {age : 21})
+
+
+// console.log(+true )
+// console.log(!"arpan" )
+
+// var a = {}
+// var b = {key : "b"}
+// var c = {key : "c"}
+
+// a[b] = 600;
+// b[c] = 700;
+// b[b] = 400
+
+// console.log(a[c]) // 600
+// console.log(a[b]) // 600
+// console.log(b[b]) // 700
+// console.log(b[c]) // 700
+// console.log(a)
+
+let str = "abcdef"
+let str2 = "12345"
+
