f4d72f42abbc4f99dacbd5727f8e9d99a4704ba8
Arpan <arpansaha0707@gmail.com>
2026-01-30 19:15:03 +0530

    today pratice

---
+++ Js PraticeDay 1/Day11.js
@@ -0,0 +1,50 @@
+// let str = "hello my name is arpan";
+let str = "hello"
+
+// function reverseStr(str) {
+//     let result = "";
+//     for (const key of str) {
+//         result = key + result;
+//     }
+//     return result;
+// }
+
+// console.log(reverseStr(str))
+
+// function revStr(str) {
+//     return str.split(" ").map(item => {
+//         let result = "";
+//         for (const key of item) {
+//             result = key + result
+//         }
+//         return result;
+//     }).join(" ")
+// }
+
+// console.log(revStr(str))
+
+
+// let str = "javascript";
+
+// function findVowel(str) {
+//     let vowel = "aeiou"
+//     return str.toLowerCase().split("").filter((item) => vowel.includes(item) ).length
+// }
+
+// console.log(findVowel(str))
+
+// function findVowel(str) {
+//     let vowel = "aeiou";
+//     let count = 0;
+//     let word = str.toLowerCase()
+//     for (let index = 0; index < word.length; index++) {
+//        if (vowel.includes(word[index])) {
+//         count++
+//        }
+//     }
+//     return count ;
+// }
+
+// console.log(findVowel(str))
+
+
