6d1e6d731f2f307e9ceb7f15b4ca7277ff5a7377
Arpan <arpansaha0707@gmail.com>
2026-01-31 10:07:28 +0530

    new questions

--- Js PraticeDay 1/Day11.js
+++ Js PraticeDay 1/Day11.js
@@ -47,4 +47,58 @@ let str = "hello"
 
 // console.log(findVowel(str))
 
+// let str1 = "messi"
 
+// function isPal(str1) {
+//     let result = str1.split("").reverse().join("")
+//     return result === str1;
+// }
+
+// console.log(isPal(str1))
+
+// function isPal(str1) {
+//     for (let index = 0; index < str1.length/2; index++) {
+      
+//         if (str1[index] !== str1[str1.length - 1 - index]) {
+//            return false 
+//         }
+//     }
+//     return true
+// }
+
+
+// console.log(isPal(str1))
+
+
+// let str1 = "silent";
+// let str2 = "listen";
+
+// function isAnangram(str1,str2) {
+//     if (str1.length !== str2.length) return false
+    
+//     let first = str1.split("").sort().join("")
+//     let second = str2.split("").sort().join("")
+
+//     return first === second
+// }
+
+// console.log(isAnangram(str1,str2))
+
+
+// function isAnnagram(str1,str2) {
+//     if (str1.length !== str2.length) return false;
+//     let obj = {};
+//     for (const key of str1) {
+//         obj[key] = (obj[key] || 0) + 1
+//     }
+    
+//     for (const key of str2) {
+//         if (!obj[key]) {
+//             --obj[key]
+//         }
+//     }
+//     return true;
+// };
+
+
+// console.log(isAnnagram("silent","listen"))
