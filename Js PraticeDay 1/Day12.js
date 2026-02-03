fb143f817b6d4ec0e751193e4576a3655fb61806
Arpan <arpansaha0707@gmail.com>
2026-02-04 03:21:01 +0530

    DSA Interview questions

--- Js PraticeDay 1/Day12.js
+++ Js PraticeDay 1/Day12.js
@@ -44,3 +44,33 @@
 // ans ()
 // ans ()
 // ans () // 4
+
+
+let prices = [1,2,5,8,3,2,8]
+
+
+function maxProfitBruteForce(prices) {
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    console.log(`\nBuying Day ${i} (Price = ${prices[i]})`);
+
+    for (let j = i + 1; j < prices.length; j++) {
+      let profit = prices[j] - prices[i];
+
+      console.log(
+        `  Selling Day ${j} (Price = ${prices[j]}) → Profit = ${profit}`
+      );
+
+      if (profit > maxProfit) {
+        maxProfit = profit;
+        console.log(`  ✅ New Max Profit Updated: ${maxProfit}`);
+      }
+    }
+  }
+
+  console.log(`\nFinal Maximum Profit = ${maxProfit}`);
+  return maxProfit;
+}
+
+maxProfitBruteForce(prices);
