67fd8caa93805425c34ce5b25441e73a3952416c
Arpan <arpansaha0707@gmail.com>
2026-02-16 17:48:08 +0530

    short question pratices

---
+++ Js PraticeDay 1/Day16.js
@@ -0,0 +1,28 @@
+// const arr = [10,18,12,15,9,11]
+
+// function sortArray(arr) {
+
+// for (let i = 0; i < arr.length - 1; i++) {
+//   for (let j = 0; j < arr.length - 1 - i; j++) {
+
+//     if (arr[j] > arr[j + 1]) {
+//       [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//     }
+
+//   }
+// }
+
+// console.log(arr);
+// }
+
+// sortArray(arr)
+
+// let arr = [10,20,30,40,50];
+// console.log(arr.splice(1,2,100))
+// console.log(arr)
+
+// const name = function () {
+//     console.log("hello");
+// };
+
+// name()
