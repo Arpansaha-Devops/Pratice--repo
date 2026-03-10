51b651cb807bb7a98a20ca07c52d05cdc0307428
Arpan <arpansaha0707@gmail.com>
2026-03-10 12:25:59 +0530

    Easy question's

---
+++ Js PraticeDay 1/Day22.js
@@ -0,0 +1,41 @@
+// Give me some example of clousures ----
+
+
+// function a() {
+//   let count = 0;
+//   return function b() {
+//     count ++ 
+//     console.log(count);
+//   }  
+// }
+
+
+
+// let c = a();
+// c()
+// c()
+// c()
+
+// for (var i = 1; i <= 3; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 1000);
+// }
+
+
+// give me the difference between filter & Find methods ----
+
+// let arr  = [1,2,3,4];
+
+// function filterMethod(arr) {
+//     return arr.filter(item => item >= 2 )
+// }
+// function findMethod(arr) {
+//     return arr.find(item => item >= 2 )
+// }
+
+// console.log(filterMethod(arr))
+// console.log(findMethod(arr))
+
+console.log([1,2] == "1,2"); // true
+console.log([1,2] === "1,2"); // false
