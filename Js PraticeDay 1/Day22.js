61e0edbcaf46c4e295692b9e9d987cc8f961bab8
Arpan <arpansaha0707@gmail.com>
2026-03-17 23:19:27 +0530

    toaday pratice

--- Js PraticeDay 1/Day22.js
+++ Js PraticeDay 1/Day22.js
@@ -38,4 +38,40 @@
 // console.log(findMethod(arr))
 
 console.log([1,2] == "1,2"); // true
-console.log([1,2] === "1,2"); // false
+// console.log([1,2] === "1,2"); // false
+
+
+// let str = "hello";
+
+// function revStr(str) {
+//     let result = "";
+//     for (const key of str) {
+//         result = key + result
+//     }
+//     return result;
+// }
+
+// console.log(revStr(str))
+
+
+let str = "racar";
+
+// function isPalstr(str) {
+//     for (let index = 0; index < str.length/2; index++) {
+       
+//         if (str[index] !== str[str.length - 1 - index]) {
+//             return false;
+//         }
+//     }
+//     return true
+// }
+
+// console.log(isPalstr(str))
+
+
+function ispalStr(str) {
+    let result = str.split("").reverse().join("")
+    return result === str
+};
+
+console.log(ispalStr(str))
