e9db2d4a260dcbbd92ab8b1a90ac4328f20923ef
Arpan <arpansaha0707@gmail.com>
2026-03-19 23:26:57 +0530

    one question solved

---
+++ Js PraticeDay 1/Day23.js
@@ -0,0 +1,47 @@
+let arr = [1,2,2,3,3,4,5,5];
+
+// function removeDuplicateArr(arr) {
+//     let result = [];
+//     for (let index = 0; index < arr.length; index++) {
+        
+//         if (!result.includes(arr[index])) {
+//             result.push(arr[index])
+//         }
+//     }
+//     return result
+// };
+
+// console.log(removeDuplicateArr(arr))
+
+
+// function removeaRr(arr) {
+//     return arr.filter((item,index,array) => array.indexOf(item) === index)
+// }
+
+// console.log(removeaRr(arr))
+
+// function reduceArr(arr) {
+//     return arr.reduce((acc, curr) => {
+//         if (!acc.includes(curr)) acc.push(curr);
+//         return acc;
+//     }, []);
+// }
+
+
+// function removeDuplicate(arr) {
+//     let result = [];
+//     let map = new Map();
+//     for (let index = 0; index < arr.length; index++) {
+//       if (!map.has(arr[index])) {
+//         map.set(arr[index])
+//         result.push(arr[index])
+//       }
+        
+//     }
+//     return result
+// }
+
+// console.log(removeDuplicate(arr))
+
+
+
